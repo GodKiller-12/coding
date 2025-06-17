17cb26eee4f2c58c482471cdf3f70c3f23d280b5
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-18 03:00:19 +0530

    Time: 3 ms (30.99%) | Memory: 22.1 MB (42.00%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& a) {
+        stack<int>st;
+        int n = a.size();
+        for(int i = 0;i<n;i++){
+            if(a[i]<0){
+            while(!st.empty() && st.top()>0 && st.top()<-a[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                st.push(a[i]);
+            }
+            else if(!st.empty() && st.top()<0){
+                st.push(a[i]);
+            }
+            else  if(st.top()==-a[i]){
+                st.pop();
+            }
+            
+            
+            }
+            else{
+                st.push(a[i]);
+            }
+        }
+        vector<int>ans;
+        while(!st.empty()){
+            ans.push_back(st.top());
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans ;
+    }
+};
