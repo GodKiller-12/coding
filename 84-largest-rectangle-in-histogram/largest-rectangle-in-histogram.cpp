607b0aa9153074216608ff61d533b46c4d08cd93
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-18 03:59:11 +0530

    Time: 58 ms (7.97%) | Memory: 96 MB (5.03%) - LeetSync

---
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>&a) {
+        //  a[i] *(nse - pse -1);
+      int n = a.size();
+        vector<pair<int,int>>nexts(n);
+        vector<pair<int,int>>prevs(n);
+        stack<pair<int,int>>st;
+        for(int i = n- 1;i>=0;i--){
+            while(!st.empty() && st.top().first>a[i]){
+                st.pop();
+            }
+            if(st.empty()){
+                nexts[i] = {-1,-1};
+            }else{
+                nexts[i] = st.top();
+            }
+            st.push({a[i],i});
+        }
+        while(!st.empty()){
+            st.pop();
+        }
+
+        for(int i = 0;i<n;i++){
+            while(!st.empty() && st.top().first>=a[i]){
+                st.pop();
+            }
+             if(st.empty()){
+                prevs[i] = {-1,-1};
+            }else{
+                prevs[i] = st.top();
+            }
+            st.push({a[i],i});
+        }
+        int MOD = 1e9+7 ;
+        long long ans = 0;
+        for(int i = 0 ;i<n;i++){
+            int left = -1;
+            int right = n;
+            if(prevs[i].second!=-1){
+                left = prevs[i].second ;
+            }
+            if(nexts[i].second!=-1){
+                right = nexts[i].second ;
+            }
+            // cout<<left<<" "<<i<<" "<<right<<endl;
+           ans = max(ans,1LL*(right-left-1)*a[i]);
+        }
+        return ans ;
+
+    }
+};
