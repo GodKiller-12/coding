045514ac254d1e61a484f88a02c115c90bdf3f27
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-21 12:11:02 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (62.50%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,42 +1,22 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
- #define Node ListNode*
+#define Node ListNode*
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head== NULL) return NULL;
-        if(head->next == NULL || head->next->next== NULL) return head ;
-        vector<int>ans ;
-        Node temp = head ;
-        ans.push_back(head->val);
-        while(temp->next!=NULL && temp->next->next!=NULL){
-            temp = temp->next->next;
-            ans.push_back(temp->val);
+        if (head == NULL || head->next == NULL || head->next->next == NULL)
+            return head;
+        Node odd = head;
+        Node even = head->next;
+        Node evenHead = even;
+
+        while (even != NULL && even->next != NULL) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
         }
-        temp = head ->next ;
-        ans.push_back(temp->val);
-         while(temp->next!=NULL && temp->next->next!=NULL){
-             temp = temp->next->next;
-            ans.push_back(temp->val);
-        }
-        // for(auto i:ans){
-        //     cout<<i<<" ";
-        // }
-        int i = 0;
-        temp = head ;
-         while(temp!=NULL){
-            temp->val = ans[i];
-            temp = temp->next;
-            i++;
-        }
-        return head ;
+
+        odd->next = evenHead;
+
+        return head;
     }
 };
