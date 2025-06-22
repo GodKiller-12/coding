a2c9725c360a1318596bd3166f8a3c5bcc08b58e
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-22 10:17:30 +0530

    Time: 1311 ms (5.02%) | Memory: 25.8 MB (5.01%) - LeetSync

---
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+map<pair<int,int>,int>mp;
+int rec(int i , int j , vector<int>&cuts ){
+    bool found = false;
+    for(int k = 0;k<cuts.size();k++){
+        if(cuts[k]>i && cuts[k]<j){
+            found = true ;
+        }
+    }
+    if(!found){
+      return 0;
+    }
+    if(mp.count({i,j})) return mp[{i,j}];
+int ans = INT_MAX ;
+    for(int k = 0;k<cuts.size();k++){
+        if(cuts[k] > i && cuts[k]<j){
+            ans = min(ans,(j-i) + rec(i,cuts[k],cuts) + rec(cuts[k],j,cuts));
+        }
+    }
+    return mp[{i,j}] = ans ;
+}
+    int minCost(int n, vector<int>& cuts) {
+        return rec(0,n,cuts);
+    }
+};
