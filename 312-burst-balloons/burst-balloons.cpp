9e76f773d2d167be01d9d64db6829f77fcd2e7af
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-22 19:14:59 +0530

    Time: 55 ms (99.06%) | Memory: 11.2 MB (94.86%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
-int dp[301][301];
-    int rec(int i , int j , vector<int>&a){
-        int n = a.size();
-        if(i>j) return 0;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-int ans = INT_MIN;
-for(int k = i;k<=j;k++){
-    ans = max(ans,a[i-1]*a[k]*a[j+1] + rec(i,k-1,a) + rec(k+1,j,a));
-}
-return dp[i][j] = ans ;
-    }
+int dp[305][305];
+    // int rec(int i , int j , vector<int>&a){
+    //     int n = a.size();
+    //     if(i>j) return 0;
+    //     if(dp[i][j]!=-1){
+    //         return dp[i][j];
+    //     }
+
+    // }
     int maxCoins(vector<int>&arr) {
         int n = arr.size();
         vector<int>a ;
@@ -22,6 +18,27 @@ return dp[i][j] = ans ;
             a.push_back(arr[i]);
         }
         a.push_back(1);
-      return rec(1,n,a);  
+    //   return rec(1,n,a);  
+
+
+    for(int i = n;i>=1;i--){
+        for(int j = 1;j<=n;j++){
+           if(i>j){
+            dp[i][j] = 0; 
+           }
+           else{
+int ans = INT_MIN;
+for(int k = i;k<=j;k++){
+    ans = max(ans,a[i-1]*a[k]*a[j+1] + dp[i][k-1] + dp[k+1][j]);
+}
+dp[i][j] = ans ;
+           }
+        }
+    }
+
+
+
+return dp[1][n]+2;
+
     }
 };
