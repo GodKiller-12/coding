8683e227f58309de16a07eed09e3d9e0217b2ce2
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-23 10:50:23 +0530

    Time: 3 ms (82.35%) | Memory: 27.6 MB (86.85%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -1,6 +1,5 @@
 class Solution {
 public:
-int dp[301][301];
     int countSquares(vector<vector<int>>&a) {
         int m = a.size();
         int n = a[0].size();
@@ -9,13 +8,13 @@ int dp[301][301];
             for(int j = 0;j<n;j++){
                 if(a[i][j]==1){
                     if(i==0 || j==0){
-                        dp[i][j] = 1;
+                        // dp[i][j] = 1;
                     }
                     else{
-                    dp[i][j] = min({dp[i-1][j],dp[i-1][j-1],dp[i][j-1]}) + 1 ;
+                    a[i][j] = min({a[i-1][j],a[i-1][j-1],a[i][j-1]}) + 1 ;
                     }
                 }
-                sum += dp[i][j];
+                sum += a[i][j];
             }
         }
         return sum ;
