0f18548c27ad6dc4ae95af4c1408594c87d43361
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-23 10:23:47 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (96.94%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.cpp
@@ -2,28 +2,30 @@ class Solution {
 public:
 int k ;
 int dp[501];
-    int rec(int level, vector<int>& a) {
-        int n = a.size();
-     if(level==n){
-        return 0;
-     }
-     if(dp[level]!=-1){
-        return dp[level];
-     }
-     int ans = INT_MIN ;
-     int maxi = INT_MIN ;
-     for(int i = level ;i<min(level+k,n);i++){
-        maxi = max(maxi,a[i]);
-        ans = max(ans,maxi *(i + 1 - level) + rec(i+1,a));
-     }
-
-     return dp[level] = max(ans,dp[level]) ;
-    }
+    // int rec(int level, vector<int>& a) {
+    //     int n = a.size();
+   
+    // }
 
     int maxSumAfterPartitioning(vector<int>& a, int tar) {
         k = tar;
         int n = a.size();
-        memset(dp,-1,sizeof(dp));
-      return rec(0,a);
+    //     memset(dp,-1,sizeof(dp));
+    //   return rec(0,a);
+    for(int level = n ;level>=0;level--){
+  if(level==n){
+        dp[level] =  0;
+     }else{
+     int ans = INT_MIN ;
+     int maxi = INT_MIN ;
+     for(int i = level ;i<min(level+k,n);i++){
+        maxi = max(maxi,a[i]);
+        ans = max(ans,maxi *(i + 1 - level) + dp[i+1]);
+     }
+     dp[level] = max(ans,dp[level]) ;
+
+     }
+    }
+    return dp[0];
     }
 };
