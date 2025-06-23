f95d2663891b2bcef7ecc7e5aadeecd7bf7193d4
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-23 11:12:47 +0530

    Time: 11 ms (98.29%) | Memory: 11.2 MB (99.23%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.cpp
@@ -1,27 +1,32 @@
 class Solution {
 public:
-int dp[101][101];
-int rec(int i , int j , vector<int>& cuts){
-   if(i>j){
-    return 0;
-   }
-   if(dp[i][j]!=-1){
-    return dp[i][j];
-   }
-   int ans = INT_MAX;
-   for(int k = i ;k<=j;k++){
-    ans = min(ans , cuts[j+1]-cuts[i-1] + rec(i,k-1,cuts) + rec(k+1,j,cuts));
-   }
-   return dp[i][j] = ans ;
-}
+int dp[105][105];
+// int rec(int i , int j , vector<int>& cuts){
+  
+// }
 
 int minCost(int n, vector<int>& cuts) {
     cuts.push_back(0);
     cuts.push_back(n);
     sort(cuts.begin(),cuts.end());
-    memset(dp,-1,sizeof(dp));
     int N = cuts.size();
-    return rec(1,N-2, cuts);
-    
+    // return rec(1,N-2, cuts);
+    for(int i  = N-2;i>=1;i--){
+        for(int j = 1;j<=N-2;j++){
+ if(i>j){
+    dp[i][j]= 0;
+    continue ;
+   }
+  
+   int ans = INT_MAX;
+   for(int k = i ;k<=j;k++){
+    ans = min(ans , cuts[j+1]-cuts[i-1] +dp[i][k-1] + dp[k+1][j]);
+   }
+    dp[i][j] = ans ;
+        }
+    }
+
+    return dp[1][N-2];
+
 }
 };
