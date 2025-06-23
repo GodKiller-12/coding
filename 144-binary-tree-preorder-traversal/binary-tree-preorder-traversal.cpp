b0df3d5935542b5594c7727c37840e45578d3f38
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-23 17:56:12 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (41.54%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -1,37 +1,40 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
- #define node TreeNode* 
- #define pb push_back
 class Solution {
 public:
+#define Node TreeNode*
+#define pb push_back
     vector<int> preorderTraversal(TreeNode* root) {
         if(root==NULL){
             return {};
         }
-        stack<node>st;
-        vector<int>ans ;
-        st.push(root);
-        while(!st.empty()){
-           node temp = st.top();
-           st.pop();
-           ans.pb(temp->val);
-           if(temp->right!=NULL){
-            st.push(temp->right);
-           }
-           if(temp->left!=NULL){
-            st.push(temp->left);
-           }
-           
+      vector<int>in;
+      vector<int>po;
+      vector<int>pre;
+      stack<pair<int,Node>>st;
+      st.push({1,root});
+      while(!st.empty()){
+        auto it = st.top();
+        st.pop();
+        if(it.first == 1){
+            pre.pb(it.second->val);
+            st.push({2,it.second});
+            if(it.second->left!=NULL){
+                st.push({1,it.second->left});
+            }
         }
-        return ans ;
+        else if(it.first == 2){
+               in.pb(it.second->val);
+            st.push({3,it.second});
+            if(it.second->right!=NULL){
+                st.push({1,it.second->right});
+            }
+        }
+        else{
+            po.pb(it.second->val);
+        }
+      }
+return pre;
+
+
+
     }
 };
