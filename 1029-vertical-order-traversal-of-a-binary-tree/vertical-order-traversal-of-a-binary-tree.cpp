c150a82943710d05873e0ea773243b41abc8900f
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-24 12:10:15 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (43.83%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,39 +1,30 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+#define Node TreeNode* 
 class Solution {
 public:
-    void dfs(TreeNode* root, int row, int col, map<int, map<int, multiset<int>>>& nodes) {
+    void rec(Node root, int x, int y, map<pair<int, int>,multiset<int>>& mp) {
         if (!root) return;
-
-        nodes[col][row].insert(root->val);
-
-        dfs(root->left, row + 1, col - 1, nodes);  // left -> col-1
-        dfs(root->right, row + 1, col + 1, nodes); // right -> col+1
+        mp[{x, y}].insert(root->val);
+        rec(root->left, x + 1, y - 1, mp);
+        rec(root->right, x + 1, y + 1, mp);
     }
 
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> nodes;
-        dfs(root, 0, 0, nodes);
+        map<pair<int, int>, multiset<int>> mp;
+        rec(root, 0, 0, mp);
 
-        vector<vector<int>> result;
-
-        for (auto& colPair : nodes) {
-            vector<int> col;
-            for (auto& rowPair : colPair.second) {
-                col.insert(col.end(), rowPair.second.begin(), rowPair.second.end());
+        int mini = INT_MAX, maxi = INT_MIN;
+        for (auto& i : mp) {
+            mini = min(mini, i.first.second);
+            maxi = max(maxi, i.first.second);
+        }
+        int n = maxi - mini;
+        vector<vector<int>> ans(n + 1);
+        for (auto& i : mp) {
+            for (int val : i.second) {
+                ans[i.first.second - mini].push_back(val);
             }
-            result.push_back(col);
         }
 
-        return result;
+        return ans;
     }
 };
