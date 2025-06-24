7fd9c7c8ef444f7feda41b3558102116a3d9c735
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-24 12:07:38 +0530

    Time: 5 ms (11.29%) | Memory: 16.7 MB (11.94%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -1,33 +1,39 @@
-#define Node TreeNode* 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    void rec(Node root, int x, int y, map<pair<int, int>, vector<int>>& mp) {
+    void dfs(TreeNode* root, int row, int col, map<int, map<int, multiset<int>>>& nodes) {
         if (!root) return;
-        mp[{x, y}].push_back(root->val);
-        rec(root->left, x + 1, y - 1, mp);
-        rec(root->right, x + 1, y + 1, mp);
+
+        nodes[col][row].insert(root->val);
+
+        dfs(root->left, row + 1, col - 1, nodes);  // left -> col-1
+        dfs(root->right, row + 1, col + 1, nodes); // right -> col+1
     }
 
     vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<pair<int, int>, vector<int>> mp;
-        rec(root, 0, 0, mp);
+        map<int, map<int, multiset<int>>> nodes;
+        dfs(root, 0, 0, nodes);
 
-        int mini = INT_MAX, maxi = INT_MIN;
-        for (auto& i : mp) {
-            mini = min(mini, i.first.second);
-            maxi = max(maxi, i.first.second);
-        }
+        vector<vector<int>> result;
 
-        int n = maxi - mini;
-        vector<vector<int>> ans(n + 1);
-
-        for (auto& i : mp) {
-            sort(i.second.begin(), i.second.end()); 
-            for (int val : i.second) {
-                ans[i.first.second - mini].push_back(val);
+        for (auto& colPair : nodes) {
+            vector<int> col;
+            for (auto& rowPair : colPair.second) {
+                col.insert(col.end(), rowPair.second.begin(), rowPair.second.end());
             }
+            result.push_back(col);
         }
 
-        return ans;
+        return result;
     }
 };
