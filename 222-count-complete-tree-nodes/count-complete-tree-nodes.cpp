5a61eea5c7573750267242ee57b57453f87bce21
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-25 08:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 31.3 MB (43.52%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -9,15 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ #define Node TreeNode*
 class Solution {
 public:
-    int func(TreeNode *node){
+    int func(Node node){
        if(!node)
          { return 0;}
+         int l = getleft(node->left);
+         int r = getright(node->right);
+         if(l == r){
+            return (1<<l) - 1 ;
+         }
         int count = 1+func(node->left) + func(node->right);
         return count ;
     }
-
+    int getleft(Node node){
+        int cnt = 1;
+        while(node){
+            cnt ++;
+            node = node->left ;
+        }
+        return cnt ;
+    }
+    int getright(Node node ){
+        int cnt = 1;
+        while(node){
+            cnt ++;
+            node = node->right;
+        }
+        return cnt ;
+    }
     int countNodes(TreeNode* root) {
         return func(root);
     }
