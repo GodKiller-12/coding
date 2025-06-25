fd79fdb638c43cfbca71fbc17cbba9a174a22e49
GodKiller-12 <149330584+GodKiller-12@users.noreply.github.com>
2025-06-25 07:41:41 +0530

    Time: 0 ms (100.00%) | Memory: 31.5 MB (16.23%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -0,0 +1,55 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+   int h ;
+     int rec(TreeNode* root){
+     if(root ==NULL){
+        return 0;
+     }
+     int ans = 0;
+     int left = 1 + rec(root->left);
+     int right = 1 + rec (root->right);
+     return max(left,right);
+    }
+
+    int func(TreeNode *node,int level){
+        if(node->left==NULL && node->right==NULL){
+            if(level==h-1){
+                return 1;
+            }
+            return 1;
+        }
+        if(node->left==NULL || node->right ==NULL){
+            return 2;
+        }
+         
+        int count = 1+func(node->left,level+1) + func(node->right,level +1 );
+
+        return count ;
+
+    }
+
+    int countNodes(TreeNode* root) {
+        if(root==NULL){
+            return 0;
+        }
+     
+        int height = rec(root);
+           h = height ;
+        int count = pow(2,height) - 1;
+        int k =  func(root,1);
+        return k ;
+       
+
+    }
+};
